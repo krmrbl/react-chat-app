6e55aafbba8fd2247579d10fb8d23a58ac36d8f3
Oliver Nero <oliver_nero11@icloud.com>
2021-07-08 07:38:54 +0800

    Added Starting Components for Login - Register

---
+++ src/components/Login/LoginForm.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './LoginForm.scss'
+
+const LoginForm = () => {
+
+
+
+    return (
+        <div className='login-container'>
+            <h1>Hello Login</h1>
+        </div>
+    )
+}
+
+export default LoginForm;
---
+++ src/components/Register/RegisterForm.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './RegisterForm.scss'
+
+const RegisterForm = () => {
+
+
+
+    return (
+        <div className='register-container'>
+            <h1>Hello Register</h1>
+        </div>
+    )
+}
+
+export default RegisterForm;
